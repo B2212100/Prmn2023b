28605eb39374908720768fdb935db6c4154701cc
b2212100 <hiraku0209@icloud.com>
2023-10-29 12:28:11 +0900

    lecture01完成

---
+++ prmn2023b/src/main/java/lecture01/Main.java
@@ -0,0 +1,21 @@
+package lecture01;
+
+public class Main {
+
+    /* mainメソッド（ C言語でいう int main(void) { ~~ } ） */
+    public static void main(String[] args) {
+        // ここに処理を書いていく
+
+        // スラッシュ2つはコメントを意味し、処理には影響しない
+        /* このようなコメントもできる */
+//        複数行をコメント化したいときは、選択して "ctrl(command) と /"
+
+        /* 実行は ctrl と shift と f10 */
+
+        /* 一区切りついたら、
+        git add .
+        git commit -m "コメント"
+        git push origin main を忘れずに
+        "コメント"部分は、「～～作成」や「～～修正」など、自由でいいですよ */
+    }
+}
