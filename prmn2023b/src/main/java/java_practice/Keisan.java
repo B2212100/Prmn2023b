9221fcdb2a3a30b62d6084838dcca25d4ff8c1bd
b2212100 <hiraku0209@icloud.com>
2023-11-05 07:12:23 +0900

    練習問題2作成

---
+++ prmn2023b/src/main/java/java_practice/Keisan.java
@@ -0,0 +1,31 @@
+package java_practice;
+
+/* 練習問題2、自由記述ソースコード
+   数値の計算、判定のみを行う */
+public class Keisan {
+
+    /* 入力された数値5つの合計値を求めるメソッド */
+    public int sum(int[] num) {
+        int sum = 0;
+        for (int nums : num) {
+            sum += nums;
+        }
+        return sum;
+    }
+
+    /* 数値5つの合計値が
+       <=49なら"Small", 50<=なら"Big", 100<=なら"Great!"
+       と表示するメソッド */
+    public void sumJudge(int sum) {
+        System.out.println("合計値 : " + sum);
+        if (sum >= 100) {
+            System.out.println("Great!");
+        } else if (sum >= 50) {
+            System.out.println("Big");
+        } else {
+            System.out.println("Small");
+        }
+        return;
+    }
+
+}
---
+++ prmn2023b/src/main/java/java_practice/Prac2.java
@@ -0,0 +1,27 @@
+package java_practice;
+
+import java.util.Scanner;
+
+/* 練習問題2、掲示ソースコード
+   ここでは変更後のみを記述する */
+public class Prac2 {
+
+    public static void main(String[] args) {
+
+        int[] num = new int[5]; /* 配列の定義、0~4まで計5つの場所を持つ */
+
+        Scanner scan = new Scanner(System.in);
+        for (int i = 0; i < 5; i++) {
+            System.out.print(i + 1 + " つ目の数字を入力してください: ");
+            num[i] = scan.nextInt();
+            System.out.println(""); /* 改行 */
+        }
+
+        /* 上記では5つの整数入力を行った
+           それに基づく合計値と判定は、Keisanクラスのメソッドで行う */
+        Keisan keisan = new Keisan();
+        int sum = keisan.sum(num);
+
+        keisan.sumJudge(sum);
+    }
+}
